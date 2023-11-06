410e630188aa227c695fb70995b3c42840ef1a10
Santiago Mosquera Mapura <s.mosquera@globant.com>
2023-11-06 13:11:40 -0500

    Discovery server is set up. Eureka server and its clients works well, all of them are registered

--- borrowing/src/main/java/com/practice/borrowing/BorrowingApplication.java
+++ borrowing/src/main/java/com/practice/borrowing/BorrowingApplication.java
@@ -2,9 +2,10 @@ package com.practice.borrowing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 public class BorrowingApplication {
